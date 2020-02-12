397dffc176e920b80b5ccc23a08bbd57ef558bda
Susana Pineda <susana.pineda.de.luelmo@gmail.com>
2020-02-12 12:39:45 +0100

    carga de archivos

---
+++ loadDirectorio.py
@@ -0,0 +1,49 @@
+import jetson.inference
+import jetson.utils
+import os
+import cv2
+import argparse
+import numpy as np
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i","--input_dataset", required = True, help = "path of the input dataset")
+ap.add_argument("-n","--net", required = True, help = " detect network")
+ap.add_argument("-t","--threshold", required = True, help = "threshold")
+
+args = vars(ap.parse_args())
+
+inputURL = args['input_dataset']
+network = args['net']
+thres = float(args['threshold'])
+
+# load the object detection model
+net = jetson.inference.detectNet(network, threshold=thres)
+
+display = jetson.utils.glDisplay()
+data = os.listdir(inputURL)
+data = np.sort(data)
+
+for img in data:
+    frame = cv2.imread(os.path.join(inputURL,img))
+    imagen, width, height = jetson.utils.loadImageRGBA(os.path.join(inputURL,img))
+
+    detections = net.Detect(imagen, width, height)
+
+    # print only detected traffic lights
+    for detection in detections:
+        if (detection.ClassID == 10):
+            top = int(detection.Top)
+            left = int(detection.Left)
+            bottom = int(detection.Bottom)
+            right = int(detection.Right)
+            frame = cv2.rectangle(frame, (right, bottom), (left, top), (255, 0, 0), 2)
+
+    cv2.imshow("img", frame)
+
+    # print all detections and tags
+    #display.RenderOnce(imagen, width, height)
+    #display.SetTitle("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
+
+
+    if cv2.waitKey(24) & 0xFF == ord('q'):
+        break
---
+++ loadVideo.py
@@ -0,0 +1,52 @@
+import jetson.inference
+import jetson.utils
+import cv2
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i","--input_dataset", required = True, help = "path of the input video")
+ap.add_argument("-n","--net", required = True, help = "detect network")
+ap.add_argument("-t","--threshold", required = True, help = "threshold")
+
+args = vars(ap.parse_args())
+
+inputURL = args['input_dataset']
+network = args['net']
+thres = float(args['threshold'])
+
+# load the object detection model
+net = jetson.inference.detectNet(network, threshold=thres)
+
+display = jetson.utils.glDisplay()
+
+cap = cv2.VideoCapture(inputURL)
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if (ret):
+        frame_rgba = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+
+        cuda_mem = jetson.utils.cudaFromNumpy(frame_rgba)
+        detections = net.Detect(cuda_mem, frame.shape[1],frame.shape[0])
+
+        for detection in detections:
+            if (detection.ClassID == 10):
+                top = int(detection.Top)
+                left = int(detection.Left)
+                bottom = int(detection.Bottom)
+                right = int(detection.Right)
+                frame = cv2.rectangle(frame, (right, bottom), (left, top), (255, 0, 0), 2)
+
+        cv2.imshow("img", frame)
+
+        # print all detections and tags
+        #display.RenderOnce(cuda_mem, frame.shape[1], frame.shape[0])
+        #display.SetTitle("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
+
+        if cv2.waitKey(24) & 0xFF == ord('q'):
+            break
+
+    else:
+        break
+
+cv2.destroyAllWindows()
